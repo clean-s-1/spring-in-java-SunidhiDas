c09b983533ad83e4565b0e038d41b9cd96290ca5
SunidhiDas <104979688+SunidhiDas@users.noreply.github.com>
2022-05-05 16:12:57 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 public class Statistics 
 {
+    public static class Stats{
+    
+    }
     public static Stats getStatistics(List<___> numbers) {
         //implement the computation of statistics here
+        Stats s = new Stats();
+        return s;
     }
 }
