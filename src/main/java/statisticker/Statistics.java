b5fdbb09d6aa6052e6f01c6d94eb5e169a1c87f7
SunidhiDas <104979688+SunidhiDas@users.noreply.github.com>
2022-05-05 16:24:47 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -5,11 +5,17 @@ import java.util.List;
 public class Statistics 
 {
     public static class Stats{
-    
+        List<Float> numbers = new ArrayList<Float>();
+        Stats(List<Float> number){
+            numbers = number;
+        }
+        float average = 0.0f;
+        float min = 0.0f;
+        float max = 0.0f;
     }
     public static Stats getStatistics(List<Float> numbers) {
         //implement the computation of statistics here
-        Stats s = new Stats();
+        Stats s = new Stats(List<Float> numbers);
         return s;
     }
 }
